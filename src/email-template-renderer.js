da2b75986f27677718aea743a828af51fbcfdb21
tmont <tommy.mont@gmail.com>
2014-12-28 21:26:20 -0800

    initial import with jade support

---
+++ src/email-template-renderer.js
@@ -0,0 +1,181 @@
+var fs = require('fs'),
+	path = require('path'),
+	juice = require('juice2'),
+	async = require('async'),
+    extMap = {
+        jade: {
+            ext: 'jade',
+            regex: /\.jade$/,
+            compile: function(contents, filePath) {
+                var options = {
+                    pretty: true,
+                    compileDebug: true,
+                    filename: filePath
+                };
+
+                return require('jade').compile(contents, options);
+            }
+        },
+        ejs: {
+            ext: 'ejs',
+            regex: /\.ejs$/,
+            compile: function(contents, filePath) {
+                return require('ejs')(contents);
+            }
+        },
+        html: {
+            ext: 'html',
+            regex: /\.html$/,
+            compile: function(contents, filePath) {
+                return function(locals) {
+                    return contents;
+                };
+            }
+        }
+    };
+
+function EmailTemplateRenderer(templatesDir, type) {
+    if (!templatesDir) {
+        throw new Error('A template directory is required');
+    }
+    if (typeof(type) === 'string') {
+        this.type = extMap[type];
+    } else if (!type) {
+        this.type = extMap.html;
+    } else {
+        this.type = type;
+    }
+
+	this.templatesDir = templatesDir || null;
+	this.templates = {};
+	this.initialized = false;
+}
+
+EmailTemplateRenderer.prototype.init = function(callback) {
+	if (this.initialized) {
+		callback();
+		return;
+	}
+
+	var self = this;
+
+	function getDirectories(next) {
+		fs.readdir(self.templatesDir, next);
+	}
+
+	function compileTemplates(dirs, next) {
+		function compileTemplate(name, next) {
+			var dirPath = path.join(self.templatesDir, name);
+
+			fs.readdir(dirPath, function(err, files) {
+				if (err) {
+					next(err);
+					return;
+				}
+
+                var typeData = self.type;
+
+				function processFile(file, next) {
+					if (!typeData.regex.test(file)) {
+						next();
+						return;
+					}
+
+					var contentType = path.basename(file, '.' + typeData.ext),
+						filePath = path.join(dirPath, file);
+
+					var options = {
+						encoding: 'utf8'
+					};
+					fs.readFile(filePath, options, function(err, contents) {
+						if (err) {
+							next(err);
+							return;
+						}
+
+						if (!self.templates[name]) {
+							self.templates[name] = [];
+						}
+
+                        try {
+                            self.templates[name].push({
+                                path: filePath,
+                                tmpl: typeData.compile(contents, filePath),
+                                type: contentType
+                            });
+
+                            next();
+                        } catch (e) {
+                            next(e);
+                        }
+					});
+				}
+
+				async.each(files, processFile, next);
+			});
+		}
+
+		function isValidDirectory(name, next) {
+			var dirPath = path.join(self.templatesDir, name);
+			fs.stat(dirPath, function(err, stat) {
+				if (err) {
+					next(false);
+					return;
+				}
+
+				next(stat.isDirectory() && name.charAt(0) !== '.');
+			});
+		}
+
+		async.filter(dirs, isValidDirectory, function(results) {
+			async.eachSeries(results, compileTemplate, function(err) {
+				self.initialized = true;
+				next(err);
+			});
+		});
+	}
+
+	async.waterfall([ getDirectories, compileTemplates ], callback);
+};
+
+EmailTemplateRenderer.prototype.render = function(template, locals, callback) {
+	var self = this;
+	this.init(function(err) {
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		var templateData = self.templates[template];
+		if (!templateData) {
+			callback(new Error('Unknown template: ' + template));
+			return;
+		}
+
+		var result = {};
+
+		function render(data, next) {
+			var html = data.tmpl(locals);
+			if (data.type === 'text') {
+				result[data.type] = html;
+				next();
+				return;
+			}
+
+			var options = {
+				url: 'file://' + data.path
+			};
+
+			juice.juiceContent(html, options, function(err, html) {
+				result[data.type] = html;
+				next(err);
+			});
+		}
+
+		async.each(templateData, render, function(err) {
+			callback(err, result);
+		});
+	});
+};
+
+module.exports = EmailTemplateRenderer;
---
+++ tests/renderer-tests.js
@@ -0,0 +1,61 @@
+var should = require('should'),
+	Renderer = require('../src/email-template-renderer'),
+	path = require('path');
+
+describe('Renderer', function() {
+	var dataDir = __dirname + '/data';
+
+	it('should require a template directory', function() {
+		function create() {
+			new Renderer();
+		}
+
+		(create).should.throwError('A template directory is required');
+	});
+
+	it('should default to html if no type is given', function() {
+		var renderer = new Renderer('foo');
+
+		renderer.should.have.property('type');
+		renderer.type.should.have.property('ext', 'html');
+	});
+
+	it('should use custom type', function() {
+		var renderer = new Renderer('foo', {
+			hello: 'world'
+		});
+
+		renderer.should.have.property('type');
+		renderer.type.should.have.property('hello', 'world');
+	});
+
+	it('should render jade template', function(done) {
+		var dir = dataDir + '/jade';
+
+		var renderer = new Renderer(dir, 'jade'),
+			locals = { hello: 'world' };
+
+		renderer.render('test1', locals, function(err, result) {
+			try {
+				should.not.exist(err);
+				result.should.have.property('index');
+				var expected = '\n\
+<html>\n\
+  <body>\n\
+    <p>Hello world</p>\n\
+  </body>\n\
+</html>\n';
+
+				result.index.should.equal(expected);
+
+				renderer.should.have.property('templates');
+				renderer.templates.should.have.property('test1');
+				renderer.templates.should.have.property('test2');
+			} catch (e) {
+				err = e;
+			}
+
+			done(err);
+		});
+	});
+});
