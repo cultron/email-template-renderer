506155b91841d1d73beca6fb485d73612dc4a0fb
tmont <tommy.mont@gmail.com>
2014-12-30 21:03:43 -0800

    formatting

--- src/email-template-renderer.js
+++ src/email-template-renderer.js
@@ -73,78 +73,72 @@ EmailTemplateRenderer.prototype.init = function(callback) {
 		function compileTemplate(name, next) {
 			var dirPath = path.join(self.templatesDir, name);
 
-			fs.readdir(
-				dirPath, function(err, files) {
-					if (err) {
-						next(err);
+			fs.readdir(dirPath, function(err, files) {
+				if (err) {
+					next(err);
+					return;
+				}
+
+				var typeData = self.type;
+
+				function processFile(file, next) {
+					if (!typeData.regex.test(file)) {
+						next();
 						return;
 					}
 
-					var typeData = self.type;
+					var contentType = path.basename(file, '.' + typeData.ext),
+						filePath = path.join(dirPath, file);
 
-					function processFile(file, next) {
-						if (!typeData.regex.test(file)) {
-							next();
+					var options = {
+						encoding: 'utf8'
+					};
+					fs.readFile(filePath, options, function(err, contents) {
+						if (err) {
+							next(err);
 							return;
 						}
 
-						var contentType = path.basename(file, '.' + typeData.ext),
-							filePath = path.join(dirPath, file);
+						if (!self.templates[name]) {
+							self.templates[name] = [];
+						}
 
-						var options = {
-							encoding: 'utf8'
-						};
-						fs.readFile(
-							filePath, options, function(err, contents) {
-								if (err) {
-									next(err);
-									return;
-								}
-
-								if (!self.templates[name]) {
-									self.templates[name] = [];
-								}
-
-								try {
-									self.templates[name].push(
-										{
-											path: filePath,
-											tmpl: typeData.compile(contents, filePath),
-											type: contentType
-										});
-
-									next();
-								} catch (e) {
-									next(e);
-								}
+						try {
+							self.templates[name].push({
+								path: filePath,
+								tmpl: typeData.compile(contents, filePath),
+								type: contentType
 							});
-					}
 
-					async.each(files, processFile, next);
-				});
+							next();
+						} catch (e) {
+							next(e);
+						}
+					});
+				}
+
+				async.each(files, processFile, next);
+			});
 		}
 
 		function isValidDirectory(name, next) {
 			var dirPath = path.join(self.templatesDir, name);
-			fs.stat(
-				dirPath, function(err, stat) {
-					if (err) {
-						next(false);
-						return;
-					}
+			fs.stat(dirPath, function(err, stat) {
+				if (err) {
+					next(false);
+					return;
+				}
 
-					next(stat.isDirectory() && name.charAt(0) !== '.');
-				});
+				next(stat.isDirectory() && name.charAt(0) !== '.');
+			});
 		}
 
-		async.filter(
-			dirs, isValidDirectory, function(results) {
-				async.eachSeries(
-					results, compileTemplate, function(err) {
-						self.initialized = true;
-						next(err);
-					});
+		async.filter(dirs, isValidDirectory, function(results) {
+			async.eachSeries(results, compileTemplate, function(err) {
+				self.initialized = true;
+				next(err);
 			});
+		});
 	}
 
 	async.waterfall([getDirectories, compileTemplates], callback);
@@ -152,45 +146,42 @@ EmailTemplateRenderer.prototype.init = function(callback) {
 
 EmailTemplateRenderer.prototype.render = function(template, locals, callback) {
 	var self = this;
-	this.init(
-		function(err) {
-			if (err) {
-				callback(err);
+	this.init(function(err) {
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		var templateData = self.templates[template];
+		if (!templateData) {
+			callback(new Error('Unknown template: ' + template));
+			return;
+		}
+
+		var result = {};
+
+		function render(data, next) {
+			var html = data.tmpl(locals);
+			if (data.type === 'text') {
+				result[data.type] = html;
+				next();
 				return;
 			}
 
-			var templateData = self.templates[template];
-			if (!templateData) {
-				callback(new Error('Unknown template: ' + template));
-				return;
-			}
+			var options = {
+				url: 'file://' + data.path
+			};
 
-			var result = {};
+			juice.juiceContent(html, options, function(err, html) {
+				result[data.type] = html;
+				next(err);
+			});
+		}
 
-			function render(data, next) {
-				var html = data.tmpl(locals);
-				if (data.type === 'text') {
-					result[data.type] = html;
-					next();
-					return;
-				}
-
-				var options = {
-					url: 'file://' + data.path
-				};
-
-				juice.juiceContent(
-					html, options, function(err, html) {
-						result[data.type] = html;
-						next(err);
-					});
-			}
-
-			async.each(
-				templateData, render, function(err) {
-					callback(err, result);
-				});
+		async.each(templateData, render, function(err) {
+			callback(err, result);
 		});
+	});
 };
 
 module.exports = EmailTemplateRenderer;
